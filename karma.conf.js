0bb49b2be0d2dfa4c2d98320128288867371808a
Dmitry <disist357@gmail.com>
2015-11-18 01:20:18 +0200

    fix(build) Hotfix build on CI.

--- karma.conf.js
+++ karma.conf.js
@@ -7,79 +7,80 @@ var _ = require('lodash');
 var wiredep = require('wiredep');
 
 var pathSrcHtml = [
-  path.join(conf.paths.src, '/**/*.html')
+    path.join(conf.paths.src, '/**/*.html')
 ];
 
 function listFiles() {
-  var wiredepOptions = _.extend({}, conf.wiredep, {
-    dependencies: true,
-    devDependencies: true
-  });
+    var wiredepOptions = _.extend({}, conf.wiredep, {
+        dependencies: true,
+        devDependencies: true
+    });
 
-  return wiredep(wiredepOptions).js
-    .concat([
-      path.join(conf.paths.tmp, '/serve/app/index.module.js')
-    ])
-    .concat(pathSrcHtml);
+    return wiredep(wiredepOptions).js
+        .concat([
+            path.join(conf.paths.tmp, '/serve/app/index.module.js')
+        ])
+        .concat(pathSrcHtml);
 }
 
-module.exports = function(config) {
+module.exports = function (config) {
 
-  var configuration = {
-    files: listFiles(),
+    var configuration = {
+        files: listFiles(),
 
-    singleRun: true,
+        singleRun: true,
 
-    autoWatch: false,
+        autoWatch: false,
 
-    ngHtml2JsPreprocessor: {
-      stripPrefix: conf.paths.src + '/',
-      moduleName: '7JsCi'
-    },
+        ngHtml2JsPreprocessor: {
+            stripPrefix: conf.paths.src + '/',
+            moduleName: '7JsCi'
+        },
 
-    logLevel: 'WARN',
+        logLevel: 'WARN',
 
-    frameworks: ['jasmine'],
+        frameworks: ['jasmine'],
 
-    browsers : ['Chrome'],
+        browsers: ['Chrome'],
 
-    plugins : [
-      'karma-chrome-launcher',
-      'karma-coverage',
-      'karma-jasmine',
-      'karma-ng-html2js-preprocessor'
-    ],
+        plugins: [
+            'karma-chrome-launcher',
+            'karma-coverage',
+            'karma-jasmine',
+            'karma-ng-html2js-preprocessor'
+        ],
 
-    coverageReporter: {
-      type : 'html',
-      dir : 'coverage/'
-    },
+        coverageReporter: {
+            type: 'html',
+            dir: 'coverage/'
+        },
 
-    reporters: ['progress']
-  };
-
-  // This is the default preprocessors configuration for a usage with Karma cli
-  // The coverage preprocessor is added in gulp/unit-test.js only for single tests
-  // It was not possible to do it there because karma doesn't let us now if we are
-  // running a single test or not
-  configuration.preprocessors = {};
-  pathSrcHtml.forEach(function(path) {
-    configuration.preprocessors[path] = ['ng-html2js'];
-  });
-
-  // This block is needed to execute Chrome on Travis
-  // If you ever plan to use Chrome and Travis, you can keep it
-  // If not, you can safely remove it
-  // https://github.com/karma-runner/karma/issues/1144#issuecomment-53633076
-  if(configuration.browsers[0] === 'Chrome' && process.env.TRAVIS) {
-    configuration.customLaunchers = {
-      'chrome-travis-ci': {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
+        reporters: ['progress']
     };
-    configuration.browsers = ['chrome-travis-ci'];
-  }
 
-  config.set(configuration);
+    // This is the default preprocessors configuration for a usage with Karma cli
+    // The coverage preprocessor is added in gulp/unit-test.js only for single tests
+    // It was not possible to do it there because karma doesn't let us now if we are
+    // running a single test or not
+    configuration.preprocessors = {};
+    pathSrcHtml.forEach(function (path) {
+        configuration.preprocessors[path] = ['ng-html2js'];
+    });
+
+    // This block is needed to execute Chrome on Travis
+    // If you ever plan to use Chrome and Travis, you can keep it
+    // If not, you can safely remove it
+    // https://github.com/karma-runner/karma/issues/1144#issuecomment-53633076
+    if (configuration.browsers[0] === 'Chrome' && process.env.TRAVIS) {
+        configuration.customLaunchers = {
+            'chrome-travis-ci': {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        };
+        console.log('>> started on travis');
+        configuration.browsers = ['Chrome_travis_ci'];
+    }
+
+    config.set(configuration);
 };
