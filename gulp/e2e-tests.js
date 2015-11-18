82a1c02954730d8e881fe92e72926a3f5f4ff595
Dmitry <disist357@gmail.com>
2015-11-19 00:47:14 +0200

    feature (e2e tests) Remove refuse

--- gulp/e2e-tests.js
+++ gulp/e2e-tests.js
@@ -8,8 +8,6 @@ var browserSync = require('browser-sync');
 
 var $ = require('gulp-load-plugins')();
 
-gulp.task('webdriver-update', $.protractor.webdriver_update);
-
 gulp.task('webdriver-standalone', $.protractor.webdriver_standalone);
 
 function runProtractor(done) {
@@ -32,5 +30,5 @@ function runProtractor(done) {
 }
 
 gulp.task('protractor', ['protractor:src']);
-gulp.task('protractor:src', ['serve:e2e', 'webdriver-update'], runProtractor);
-gulp.task('protractor:dist', ['serve:e2e-dist', 'webdriver-update'], runProtractor);
+gulp.task('protractor:src', ['serve:e2e'], runProtractor);
+gulp.task('protractor:dist', ['serve:e2e-dist'], runProtractor);
